908d5d72daa25560a42e8dd2409280a84e71c139
Muhammed Rashid M P <alrashid4star@gmail.com>
2023-03-23 02:19:29 +0530

    ui & functionlity  ✨

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:rive_splash_screen/rive_splash_screen.dart';
+import 'package:the_grid/screens/input_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +14,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primaryColor: Colors.grey[900]),
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        primaryColor: Colors.grey[900],
+        textTheme: GoogleFonts.workSansTextTheme().copyWith(
+          headline6: TextStyle(
+            color: Colors.grey[900],
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
       home: Container(
-        color: Colors.grey[900],
+        color: Colors.white,
+        child: SplashScreen.navigate(
+          name: 'assets/the_grid.riv',
+          next: (_) => const InputScreen(),
+          until: () => Future.delayed(const Duration(microseconds: 2400)),
+          startAnimation: 'light_up',
+        ),
       ),
     );
   }
